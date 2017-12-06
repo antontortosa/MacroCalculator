db2ec3f838527d14e1ba6e81b6e12d5e2ceaab20
Antonio Tortosa Garrido <atortosa365@gmail.com>
2017-12-06 16:44:21 +0100

    funcionalidad añadir comida e historial
    no tiene vistas decentes aun pero la logica interna funciona

--- MacroCalculator/app_1/urls.py
+++ MacroCalculator/app_1/urls.py
@@ -12,4 +12,9 @@ urlpatterns = [
     url(r'^reg_user/(?P<user_id>[0-9]+)/$', views.reg_user, name='reg_user'),
     # ex: /reg_user/5/history/
     url(r'^reg_user/(?P<user_id>[0-9]+)/history/$', views.history, name='history'),
+	# ex: /reg_user/5/add_food
+    url(r'^reg_user/(?P<user_id>[0-9]+)/add_food/$', views.add_food, name='add_food'),
+    # ex: /reg_user/5/add_food/4/add_ingredient
+    url(r'^reg_user/(?P<user_id>[0-9]+)/add_food/(?P<item_id>[0-9]+)/add_ingredient$', views.add_ingredient, name='add_ingredient'),
+    
 ]		
