e16fcab3965deefb398fb82bf60dc0b6598535df
antontortosa <atortosa365@gmail.com>
2017-12-07 17:23:33 +0100

    refactor de nombres de modelos

--- MacroCalculator/app_1/models.py
+++ MacroCalculator/app_1/models.py
@@ -9,11 +9,11 @@ class Profile(models.Model):
     # "usuario django" con el que se asocia -> https://docs.djangoproject.com/en/1.11/ref/contrib/auth/#fields
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     # añadir campos no presentes en User
-    date_birth = models.DateField()
-    country = models.CharField(max_length=30)
-    city = models.CharField(max_length=30)
-    cp = models.IntegerField()
-    tags = models.CharField(max_length=180)
+    date_birth = models.DateField(default=None, blank=True, null=True)
+    country = models.CharField(max_length=30,default=None, blank=True, null=True)
+    city = models.CharField(max_length=30,default=None, blank=True, null=True)
+    cp = models.IntegerField(default=None, blank=True, null=True)
+    tags = models.CharField(max_length=180,default=None, blank=True, null=True)
 
 
 @receiver(post_save, sender=User)
@@ -24,14 +24,14 @@ def create_user_profile(sender, instance, created, **kwargs):
 
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
-    instance.Profile.save()
+    instance.profile.save()
 
 
     # def __str__(self):
     #	return self.first_name+', '+self.last_name+'\n'+self.email+'\n'+self.city+', '+str(self.cp)+'\n'+self.country
 
 
-class Items(models.Model):
+class Item(models.Model):
     name = models.CharField(max_length=30, unique="true")
     calories = models.FloatField(default=0.0)
     tot_fat = models.FloatField(default=0.0)
@@ -43,13 +43,13 @@ class Items(models.Model):
     sodium = models.FloatField(default=0.0)
 
 
-class Histories(models.Model):
+class History(models.Model):
     usuario = models.ForeignKey(Profile, on_delete=models.CASCADE)
-    item = models.ForeignKey(Items, on_delete=models.CASCADE)
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
     date_consumed = models.DateField()
 
 
-class Objectives(models.Model):
+class Objective(models.Model):
     usuario = models.ForeignKey(Profile, on_delete=models.CASCADE)
     calories_obj = models.IntegerField()
     carbs_obj = models.IntegerField()
@@ -57,13 +57,13 @@ class Objectives(models.Model):
     fat_obj = models.IntegerField()
 
 
-class Ingredients(models.Model):
-    item = models.ForeignKey(Items, on_delete=models.CASCADE)
+class Ingredient(models.Model):
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
     name = models.CharField(max_length=30)
     amount = models.CharField(max_length = 15)
 
 class ItemForm(ModelForm):
 	class Meta:
-		model = Items
+		model = Item
 		fields = ['name']
 
