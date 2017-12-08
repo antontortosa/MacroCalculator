2858706e60a8d1bc0bbd8c6a06c1e31ba0932b8a
antontortosa <atortosa365@gmail.com>
2017-12-08 12:57:51 +0100

    Vista de historial + CSS

--- MacroCalculator/app_1/admin.py
+++ MacroCalculator/app_1/admin.py
@@ -3,9 +3,10 @@ from django.contrib import admin
 # Register your models here.
 from django.contrib import admin
 
-from .models import Profile, Item, Objective, Ingredient
+from .models import Profile, Item, Objective, Ingredient, History
 
 admin.site.register(Profile)
 admin.site.register(Item)
 admin.site.register(Objective)
 admin.site.register(Ingredient)
+admin.site.register(History)
